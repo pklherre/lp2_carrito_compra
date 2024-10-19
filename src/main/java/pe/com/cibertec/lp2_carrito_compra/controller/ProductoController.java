2b59616439ee7a160dc506ccf6bafed71d002c57
unknown <SUITE@SUITECE309-SERV.cibertec.edu.pe>
2024-10-19 18:36:35 -0500

    chore: add logout

--- src/main/java/pe/com/cibertec/lp2_carrito_compra/controller/ProductoController.java
+++ src/main/java/pe/com/cibertec/lp2_carrito_compra/controller/ProductoController.java
@@ -1,9 +1,17 @@
 package pe.com.cibertec.lp2_carrito_compra.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +25,7 @@ import pe.com.cibertec.lp2_carrito_compra.model.entity.ProductoEntity;
 import pe.com.cibertec.lp2_carrito_compra.model.entity.UsuarioEntity;
 import pe.com.cibertec.lp2_carrito_compra.service.ProductoService;
 import pe.com.cibertec.lp2_carrito_compra.service.UsuarioService;
+import pe.com.cibertec.lp2_carrito_compra.service.impl.PdfService;
 
 @Controller
 public class ProductoController {
@@ -26,6 +35,9 @@ public class ProductoController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+	private PdfService pdfService;
 	
 	@GetMapping("/menu")
 	public String mostrarMenu(HttpSession sesion, Model model) {
@@ -88,6 +100,46 @@ public class ProductoController {
 		sesion.setAttribute("carrito", productos);
 		
 		return "redirect:/menu";
+	}
+	
+	@GetMapping("/generar_pdf")
+	public ResponseEntity<InputStreamResource>generarPdf(HttpSession sesion) 
+			throws IOException{
+		// 1. Extraer información para la base de datos
+		List<Pedido>productoSesion = null;
+		if(sesion.getAttribute("carrito") == null) {
+			productoSesion = new ArrayList<>();
+		}else {
+			productoSesion = (List<Pedido>) sesion.getAttribute("carrito");
+		}
+		
+		// Ver carrito con datos
+		List<DetallePedidoEntity>detallePedidoEntity = new ArrayList<>();
+		Double totalPedido = 0.0;
+		
+		for(Pedido pedido: productoSesion) {
+			DetallePedidoEntity det = new DetallePedidoEntity();
+			ProductoEntity pro = productoService.buscarProductoPorId(
+					pedido.getProductoId());
+			det.setProductoEntity(pro);
+			det.setCantidad(pedido.getCantidad());
+			detallePedidoEntity.add(det);
+			totalPedido += pedido.getCantidad() * pro.getPrecio();
+		}
+		Map<String, Object> datosPdf = new HashMap<String, Object>();
+		datosPdf.put("factura", detallePedidoEntity);
+		datosPdf.put("precio_total", totalPedido);
+		
+		ByteArrayInputStream pdfBytes = pdfService.generarPdf("template_pdf", 
+				datosPdf);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "inline; filename=productos.pdf");
+		
+		return ResponseEntity.ok()
+				.headers(headers)
+				.contentType(MediaType.APPLICATION_PDF)
+				.body(new InputStreamResource(pdfBytes));
+		
 	}
 	
 	
